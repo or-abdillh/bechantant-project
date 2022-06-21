8b72c56534f8fd4d71560e7b1d2d60077c1bf2e7
Oka R. Abdillah <hans.abdillh05@gmail.com>
2022-06-22 01:26:01 +0800

    Create Lists Tier

---
+++ src/components/AboutOurTiers.vue
@@ -0,0 +1,14 @@
+<template>
+  <main class="mt-12">
+    <sub-section-title title="Don't forget to increase your tier" subtitle="We really appreciate your contribution here, enjoy the results"></sub-section-title>
+    <!-- List our tiers -->
+    <lists-tier></lists-tier>
+  </main>
+</template>
+
+<script setup>
+
+import SubSectionTitle from '@component/SubSectionTitle.vue'
+import ListsTier from '@component/ListsTier.vue'
+
+</script>
---
+++ src/components/ListsTier.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+    <template v-for="list in 4" :key="list">
+      <section 
+        :class="list % 2 === 0 ? 'md:flex-row-reverse md:text-right border-r pr-2 md:pr-8' : 'md:flex-row border-l pl-2 md:pl-8'"
+        class="flex md:gap-8 items-center flex-col mb-6 rounded-lg">
+        <!-- Thumbnail -->
+        <span class="w-4/12 md:w-2/12">
+          <img src="/tiers/elite.jpg" alt="Tier Elite" class="border border-gray-300 rounded-full mb-2">
+        </span>
+        <!-- Contents -->
+        <div>
+          <h1 class="font-antic-slab text-indigo-500 font-bold text-lg">Tier Elite</h1>
+          <small class="text-gray-400">Have 100 Bekantan Hamox NFT's</small>
+          <ul class="text-gray-300">
+            <li>Id, consequat id, lobortis nec, <i class="fa-solid fa-check text-indigo-500 mx-2"></i></li>
+            <li>Id, consequat id, lobortis nec, <i class="fa-solid fa-check text-indigo-500 mx-2"></i></li>
+            <li>Id, consequat id, lobortis nec, <i class="fa-solid fa-check text-indigo-500 mx-2"></i></li>
+            <li>Id, consequat id, lobortis nec, <i class="fa-solid fa-check text-indigo-500 mx-2"></i></li>
+          </ul>
+        </div>
+      </section>
+    </template>
+  </main>
+</template>
