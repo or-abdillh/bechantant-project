bcf41e13b45d1ac8763b526e8e4f790ce78319a9
Oka R. Abdillah <hans.abdillh05@gmail.com>
2022-06-24 23:26:00 +0800

    Update lists tier

--- src/components/ListsTier.vue
+++ src/components/ListsTier.vue
@@ -6,17 +6,18 @@
         class="flex md:gap-8 items-center flex-col mb-6 rounded-lg">
         <!-- Thumbnail -->
         <span class="w-4/12 md:w-2/12">
-          <img src="/tiers/elite.jpg" alt="Tier Elite" class="border border-gray-300 rounded-full mb-2">
+          <img src="/nft-asset-2.jpg" alt="Tier Elite" class="border border-gray-300 rounded-full mb-2">
         </span>
         <!-- Contents -->
         <div>
           <h1 class="font-antic-slab text-gray-100 font-bold text-lg">Tier Elite</h1>
           <small class="text-gray-400">Have 100 Bekantan Hamox NFT's</small>
           <ul class="text-gray-300">
-            <li>Id, consequat id, lobortis nec, <i class="fa-solid fa-check text-gray-300 mx-2"></i></li>
-            <li>Id, consequat id, lobortis nec, <i class="fa-solid fa-check text-gray-300 mx-2"></i></li>
-            <li>Id, consequat id, lobortis nec, <i class="fa-solid fa-check text-gray-300 mx-2"></i></li>
-            <li>Id, consequat id, lobortis nec, <i class="fa-solid fa-check text-gray-300 mx-2"></i></li>
+            <template v-for="check in 4" :key="check">
+              <li 
+                :class="list % 2 === 0 ? 'md:flex-row-reverse' : ''"
+                class="flex gap-1 items-center"><i class="fa-solid fa-check text-gray-300 mx-2"></i> Idle, consequat id, lobortis nec</li>
+            </template>
           </ul>
         </div>
       </section>
